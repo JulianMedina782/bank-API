98b192324274eaab84ea3e968833aeb80fee421b
Julian Medina <julianestebanmg17@gmail.com>
2026-06-07 10:59:50 -0500

    feat: estructura inicial del proyecto Bank API

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.database import engine, Base  
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI(
+    title="Bank API",       
+    description= "API bancaria con Python, FastAPI y PostgreSQL",
+    version="1.0.0"
+)
+@app.get("/")
+def read_root():
+    return {"message": "Bank API funcionando"}
